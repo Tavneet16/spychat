061969dd5f5164de4bedb1ef8831c2ef03a98e17
Sahib <Sahib>
2017-09-05 17:56:56 +0530

    Basic conditional statement

--- spychat/main.py
+++ spychat/main.py
@@ -1,5 +1,5 @@
 print 'Let\'s et started'
-spy_name = raw_input('What is your name ?')
-spy_salutation = raw_input("What should we call you ?")
+spy_name = raw_input('What is your name ? ')
+spy_salutation = raw_input("What should we call you ? ")
 spy_name = spy_salutation + ' ' + spy_name
-print 'Welcome' + spy_name + 'Glad to see you'
+print 'Welcome ' + spy_name +  ' Glad to see you'
---
+++ spychat/main1.py
@@ -0,0 +1,11 @@
+print 'Let\'s get Started....'
+spy_name = raw_input('What is your name ? ')
+
+if len(spy_name) > 0 :
+    spy_salutation = raw_input("What should we call you ? ")
+    spy_name = spy_salutation + ' ' + spy_name
+    print 'Welcome ' + spy_name + ' Glad to see you...'
+
+
+
+
